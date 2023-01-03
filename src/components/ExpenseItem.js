f98220177f43817c1ebaba162b24acb98f85f6b3
Osama Munir <osama.munir@nuffieldhealth.com>
2023-01-03 22:18:16 +0000

    Up to ep 35

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2022,0,3);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = 540.65;
+
     return (
-      <h1>This is Expense Item</h1>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">£{expensePrice}</div>
+        </div>
+      </div>
     );
   }
   
