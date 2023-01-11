205c537ac0a5057d86945738ab095513d060c294
Osama Munir <osama.munir@nuffieldhealth.com>
2023-01-11 22:39:07 +0000

    ep.46

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,22 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-    return (
-      <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">£{props.amount}</div>
-        </div>
-      </Card>
-    );
+
+  const buttonClickHandler = () => {
+    console.log("This button has been clicked");
   }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
+      </div>
+      <button onClick={buttonClickHandler}>TESTING</button>
+    </Card>
+  );
+}
   
-  export default ExpenseItem;
+export default ExpenseItem;
   
