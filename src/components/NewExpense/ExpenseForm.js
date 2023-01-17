1b587e645f9ba72dd5a4541f236ddefe0c354436
Osama Munir <osama.munir@nuffieldhealth.com>
2023-01-17 19:37:48 +0000

    ep.57 two way binding - remove value after submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,6 +49,9 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredDate('');
+        setEnteredAmount('');
     }
 
     return(
@@ -56,15 +59,31 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input 
+                        type="text" 
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input 
+                        type="number" 
+                        min="0.01" 
+                        step="0.01" 
+                        value={enteredAmount}
+                        onChange={amountChangeHandler} 
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2020-01-01" max="2024-01-01" onChange={dateChangeHandler} />
+                    <input 
+                        type="date" 
+                        min="2020-01-01" 
+                        max="2024-01-01" 
+                        value={enteredDate}
+                        onChange={dateChangeHandler} 
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
